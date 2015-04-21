9447c6a9018df609df806f0f171d2bbf045865a0
Nir <niraltmark@gmail.com>
2015-04-22 01:44:38 +0300

    Adding endpoint for notifying after upload completes

---
+++ www/img/Lipsyncly_files/app.js
@@ -0,0 +1,30 @@
+// Ionic Starter App, v0.9.20
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic','webcam'])
+
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+})
+
+.directive('noScroll', function($document) {
+
+  return {
+    restrict: 'A',
+    link: function($scope, $element, $attr) {
+
+      $document.on('touchmove', function(e) {
+        e.preventDefault();
+      });
+    }
+  }
+})
+
+.controller('MainCtrl', function($scope, $http) {
+  console.log("sd");
+});
